dbf7523a39953617863624388b4c6158fa7e684d
Jhordan <Jhordan@DESKTOP-R6FKEMN>
2021-11-29 19:14:26 -0500

    Add project files.

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,58 @@
+﻿using DniApi.DataContext;
+using DniApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DniApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly PersonContext _db;
+        public PersonController(PersonContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+      
+        public async Task<IActionResult> GetPerson()
+        {
+            var lista = await _db.Person.OrderBy(c => c.Nombre).ToListAsync();
+            return Ok(lista);
+        }
+        [HttpPost]
+       
+        public async Task<IActionResult> CrearPerson([FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest(ModelState);
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await _db.AddAsync(person);
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+        [HttpGet("{Dni:int}")]
+        
+        public async Task<IActionResult> GetPerson(string Dni)
+        {
+            var obj = await _db.Person.FirstOrDefaultAsync(c => c.Dni == Dni);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return Ok(obj);
+        }
+    }
+}
---
+++ DataContext/PersonContext.cs
@@ -0,0 +1,14 @@
+﻿using DniApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DniApi.DataContext
+{
+    public class PersonContext : DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Person> Person { get; set; }
+    }
+}
